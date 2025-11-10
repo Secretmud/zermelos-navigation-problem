fixed width to be 1, fixed a fault that caused the fidelity calculation to check againts the wrong ground state.

src/lib/__init__.py
@@ -4,7 +4,7 @@ import numpy as np
 Z = np.array([[1, 0, 0], [0, 0, 0], [0, 0, -1]])
 X = 1/np.sqrt(2)*np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 
-D = 10
+D = 1
 N = 2
 smax = 0.9
 v = 1

src/lib/time.py
@@ -4,7 +4,7 @@ from lib import smax, D, v
 
 def S(x):
     # return smax*np.exp(-(x-D/2)**4/5000)
-    return smax*np.exp(-(x-D)**2/50)
+    return smax * np.exp(-(x - D/13)**2)
 
 scale = 100
 