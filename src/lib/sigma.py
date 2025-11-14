cf6be66e3c62e28e2fee23cd2b349c6d26befa7b
Tor Kristian Dahle <tk@torkristian.tech>
2025-11-14 09:56:34 +0100

    updated notebook

--- src/lib/sigma.py
+++ src/lib/sigma.py
@@ -6,5 +6,5 @@ def sigma(N, k, matrix=Z, basis=3):
     return np.kron(np.kron(np.identity(basis**k), matrix), np.identity(basis**(N-k-1)))
 
 
-def sigma_vec(N, k, matrix=np.array([1, 0, -1]), basis=3):
-    return np.kron(np.kron(np.ones(basis**k), matrix), np.ones(basis**(N-k-1)))
+def sigma_vec(N, k, vector=np.array([1, 0, -1]), basis=3):
+    return np.kron(np.kron(np.ones(basis**k), vector), np.ones(basis**(N-k-1)))
